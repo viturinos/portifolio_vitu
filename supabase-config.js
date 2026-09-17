07ea178185de81d5fcaf482bb6cdc5ff0ec529b2
viturinos <victor.e.sousa7@aluno.senai.br>
2026-09-17 17:29:06 -0300

    atualizando banco

--- supabase-config.js
+++ supabase-config.js
@@ -7,5 +7,5 @@
    SUPABASE_ANON_KEY  = anon public key
    ============================================= */
 
-const SUPABASE_URL = 'https://cmbvqierwajlytzanvka.supabase.co/rest/v1/';
+const SUPABASE_URL = 'https://cmbvqierwajlytzanvka.supabase.co';
 const SUPABASE_ANON_KEY = 'sb_publishable_igfxEhZqBL8hlEriT3VNVw_D1gkS8CC';
